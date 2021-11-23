db1027904e1da1156e97d1f6ca824eaefc4d6d80
juankcba <juangblade@hotmail.com>
2021-11-22 21:17:57 -0300

    cambios

--- server.py
+++ server.py
@@ -5,15 +5,6 @@ import tornado.httpserver
 import tornado.websocket
 import tornado.ioloop
 import tornado.web
-import RPi.GPIO as GPIO
-
-#Initialize Raspberry PI GPIO
-GPIO.setmode(GPIO.BOARD)
-
-GPIO.setup(11, GPIO.OUT)
-GPIO.setup(13, GPIO.OUT)
-GPIO.setup(16, GPIO.OUT)
-GPIO.setup(18, GPIO.OUT)
 
 #Tornado Folder Paths
 settings = dict(
@@ -38,25 +29,7 @@ class WSHandler(tornado.websocket.WebSocketHandler):
   def on_message(self, message):
     print ('[WS] Incoming message:'), message
     
-    if message == "on_g":
-      GPIO.output(16, True)
-    if message == "off_g":
-      GPIO.output(16, False)
-      
-    if message == "on_r":
-      GPIO.output(18, True)
-    if message == "off_r":
-      GPIO.output(18, False)
-      
-    if message == 'on_b':
-      GPIO.output(11 , True)
-    if message == 'off_b':
-      GPIO.output(11 , False)
-      
-    if message == 'on_w':
-      GPIO.output(13 , True)
-    if message == 'off_w':
-      GPIO.output(13 , False)
+    
 
   def on_close(self):
     print ('[WS] Connection was closed.')
@@ -79,6 +52,6 @@ if __name__ == "__main__":
 
     except:
         print ("Exception triggered - Tornado Server stopped.")
-        GPIO.cleanup()
+        
 
 #End of Program
